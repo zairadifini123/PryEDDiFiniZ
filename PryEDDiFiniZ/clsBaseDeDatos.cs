72a9b085681dfb7487916330a6b98f05255f95c3
Zaira Di Fini <zairadifini123@gmail.com>
2026-06-01 14:00:28 -0300

    .

---
+++ PryEDDiFiniZ/clsBaseDeDatos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms; 
+
+namespace PryEDDiFiniZ
+{
+    internal class clsBaseDeDatos
+    {
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbCommand comando = new OleDbCommand();
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string CadenaConexion1 = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Libreria.mdb";
+        private string CadenaConexion2 = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Libreria.mdb";
+
+        public void Listar(DataGridView Grilla)
+        {
+            try
+            {
+                conexion.ConnectionString = CadenaConexion1;
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = "Libro";
+
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, "Libro");
+
+                Grilla.DataSource = null;
+                Grilla.DataSource = DS.Tables["Libros"];
+
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("e.ToString");
+            }
+        }
+
+    }
+}
